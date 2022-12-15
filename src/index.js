38219f2c98d5c5ed7903a3f85ce3edc444f6d30c
Vladimir <workvladster@gmail.com>
2022-12-14 20:50:43 -0800

    packages update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App';
 
@@ -20,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
